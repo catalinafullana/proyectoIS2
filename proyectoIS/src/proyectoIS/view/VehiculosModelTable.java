bb9bf5e59a47b79551b6a0275ee46de9861e4173
Catalina F. Mora <catalinafullanamora@gmail.com>
2024-04-08 22:39:18 +0200

    view vehículos

--- proyectoIS/src/proyectoIS/view/VehiculosModelTable.java
+++ proyectoIS/src/proyectoIS/view/VehiculosModelTable.java
@@ -1,17 +1,22 @@
 package proyectoIS.view;
 
+import proyectoIS.modelo_de_dominio.Vehiculo;
+
 import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VehiculosModelTable extends AbstractTableModel {
-    String[] _headers ={"Matricula", "Tipo", "Modelo"};
+    String[] _headers = {"Matricula", "Tipo", "Modelo"};
+    List<Vehiculo> vehiculos;
 
     public VehiculosModelTable() {
-
+        vehiculos = new ArrayList<Vehiculo>();
     }
 
     @Override
     public int getRowCount() {
-        return 0;
+        return vehiculos.size();
     }
 
     @Override
@@ -21,6 +26,18 @@ public class VehiculosModelTable extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return null;
+        Object ret = null;
+        Vehiculo vehiculo = vehiculos.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                ret = vehiculo.get_matricula();
+                break;
+            case 1:
+                ret = vehiculo.get_tipo_vehiculo();
+                break;
+            case 2:
+                ret = vehiculo.get_modelo();
+        }
+        return ret;
     }
 }
