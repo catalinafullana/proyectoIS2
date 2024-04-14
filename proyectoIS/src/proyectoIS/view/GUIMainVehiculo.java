d49365040197dfb8233922c5b75c3af82a02bace
Alai Miranda Blanco Lascurain <alaimbl@gmail.com>
2024-04-14 15:45:41 +0200

    Rebuild de MainVehiculo

--- proyectoIS/src/proyectoIS/view/GUIMainVehiculo.java
+++ proyectoIS/src/proyectoIS/view/GUIMainVehiculo.java
@@ -1,5 +1,6 @@
 package proyectoIS.view;
 
+import proyectoIS.Main;
 import proyectoIS.controller.ControladorVehiculo;
 
 import javax.swing.*;
@@ -8,9 +9,13 @@ import java.awt.*;
 public class GUIMainVehiculo extends JPanel implements VehiculoObserver{
 
     ControladorVehiculo controladorVehiculo;
-    JButton eraseVehicle;
+    private JButton home;
+
+    //JButton eraseVehicle;
     JButton addVehicle;
-    JTextField search;
+    JTextField search_matricula;
+    JTextField search_modelo;
+    JTextField search_tipo;
 
     GUIAltaVehiculo guiAltaVehiculo;
 
@@ -22,45 +27,99 @@ public class GUIMainVehiculo extends JPanel implements VehiculoObserver{
 
     private void init_GUI() {
         setLayout(new BorderLayout());
+        //setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-        createHeader();
+        toolbar();
+        JPanel panelPrincipal = new JPanel();
+        createHeader(panelPrincipal);
+        add(panelPrincipal);
 
         VehiculosModelTable model = new VehiculosModelTable();
         JTable vehiculos = new JTable(model);
         vehiculos.setVisible(true);
     }
 
-    private void createHeader() {
-        addSeparator(this, new Dimension(MainWindow.width, 40), JToolBar.Separator.HORIZONTAL);
+    private void toolbar() {
+        JToolBar toolbar = new JToolBar();
+        toolbar.setBackground(Color.decode("#274060"));
+        toolbar.setFloatable(false);
+        toolbar.setRollover(true);
+        toolbar.setPreferredSize(new Dimension(MainWindow.width, 50));
+
+        home = createButton("Home", "resources/icons/logo_azul_30x30.png", new Dimension(30,30));
+        toolbar.add(home);
+        //toolbar.addSeparator();
+        toolbar.add(Box.createHorizontalStrut(10));
+
+
+        //JLabel header = new JLabel("Vehículos");
+        JLabel header = new JLabel("<html><font size='25' color=white> Vehículos </font></html>");
+        header.setFont(new Font("Arial", Font.BOLD, 25));
+        header.setPreferredSize(new Dimension((int) (MainWindow.width*0.4), 50));
+        toolbar.add(header);
+
+        add(toolbar, BorderLayout.PAGE_START);
+    }
+
+    private void createHeader(JPanel p) {
+        //addSeparator(p, new Dimension(MainWindow.width, 10), JToolBar.Separator.HORIZONTAL);
         JPanel headerPanel = new JPanel();
-        headerPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        headerPanel.setLayout(new FlowLayout());
+        //headerPanel.setPreferredSize(new Dimension((int)(MainWindow.width*0.9), 40));
+        //headerPanel.setPreferredSize(new Dimension((int)(MainWindow.width*0.9), headerPanel.getPreferredSize().height));
 
 
+        /*
         JLabel header = new JLabel("Vehículos");
         header.setFont(new Font("Arial", Font.BOLD, 25));
         header.setPreferredSize(new Dimension((int) (MainWindow.width*0.4), 50));
 
         headerPanel.add(header);
-        createButtonsInHeader(headerPanel);
-        headerPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        add(headerPanel, BorderLayout.NORTH);
+         */
+        createButtonsInHeader(headerPanel);
+        headerPanel.setBorder(BorderFactory.createLineBorder(Color.decode("#274060")));
+
+        p.add(headerPanel, BorderLayout.NORTH);
     }
 
     private void createButtonsInHeader(JPanel headerPanel) {
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 
-        search = new JTextField("Buscar");
-        search.setPreferredSize(new Dimension(200, 30));
-        search.addActionListener(e-> {
-            //actualizar tabla en funcion de los contenidos del search
+        addVehicle = new JButton(new ImageIcon("resources/icons/add.png"));
+        addVehicle.setPreferredSize(new Dimension(40, 40));
+        addVehicle.addActionListener(e-> {
+            //abrir formulario crear
         });
-        buttonPanel.add(search);
-
+        buttonPanel.add(addVehicle);
         addSeparator(buttonPanel, new Dimension(10, 20), JToolBar.Separator.VERTICAL);
 
+
+        search_matricula = new JTextField("Matrícula");
+        search_matricula.setPreferredSize(new Dimension(100, 30));
+        search_matricula.addActionListener(e-> {
+            //actualizar tabla en funcion de los contenidos del search
+        });
+        buttonPanel.add(search_matricula);
+
+        search_modelo = new JTextField("Modelo");
+        search_modelo.setPreferredSize(new Dimension(100, 30));
+        search_modelo.addActionListener(e-> {
+            //actualizar tabla en funcion de los contenidos del search
+        });
+        buttonPanel.add(search_modelo);
+
+        search_tipo = new JTextField("Tipo");
+        search_tipo.setPreferredSize(new Dimension(100, 30));
+        search_tipo.addActionListener(e-> {
+            //actualizar tabla en funcion de los contenidos del search
+        });
+        buttonPanel.add(search_tipo);
+
+        addSeparator(buttonPanel, new Dimension(10, 20), JToolBar.Separator.VERTICAL);
+        /*
         eraseVehicle = new JButton(new ImageIcon("resources/icons/erase.png"));
         eraseVehicle.setPreferredSize(new Dimension(40, 40));
         eraseVehicle.addActionListener(e-> {
@@ -75,8 +134,17 @@ public class GUIMainVehiculo extends JPanel implements VehiculoObserver{
         });
         buttonPanel.add(addVehicle);
 
+         */
+
         headerPanel.add(buttonPanel);
 
+    }
+    private JButton createButton(String toolTipText, String iconFileNamePath, Dimension iconSize){
+        JButton b = new JButton();
+        b.setToolTipText(toolTipText);
+        b.setIcon(new ImageIcon(iconFileNamePath));
+        b.setPreferredSize(iconSize);
+        return  b;
     }
 
     private void addSeparator(JPanel p, Dimension dim, int orientation) {
@@ -84,5 +152,6 @@ public class GUIMainVehiculo extends JPanel implements VehiculoObserver{
         s.setPreferredSize(dim);
         p.add(s);
     }
+
 
 }
