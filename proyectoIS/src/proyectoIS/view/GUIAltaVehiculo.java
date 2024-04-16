ce3c9bdc3ade860b5b8c06f5a24518f2aaad62a1
Alai Miranda Blanco Lascurain <alaimbl@gmail.com>
2024-04-16 11:37:16 +0200

    Rebuild de GUIAltaVehiculo 2

--- proyectoIS/src/proyectoIS/view/GUIAltaVehiculo.java
+++ proyectoIS/src/proyectoIS/view/GUIAltaVehiculo.java
@@ -1,6 +1,7 @@
 package proyectoIS.view;
 
 import proyectoIS.controller.ControladorVehiculo;
+import proyectoIS.misc.TipoCarnet;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,7 +11,7 @@ public class GUIAltaVehiculo extends JPanel implements VehiculoObserver{
     MainWindow mainWindow;
     GUIMainVehiculo guiMainVehiculo;
     JTextPane _matricula_vehiculo_text_field;
-    JTextPane _tipo_vehiculo_text_field;
+    JComboBox _tipo_vehiculo;
     JTextPane _modelo_vehiculo_text_field;
     JButton _guardar;
     JButton _anyadir;
@@ -37,14 +38,26 @@ public class GUIAltaVehiculo extends JPanel implements VehiculoObserver{
         panelPrincipal.setPreferredSize(new Dimension((int)(MainWindow.width * 0.6), (int)(MainWindow.height * 0.5)));
 
         JPanel panelDatos = new JPanel(new GridLayout(3, 2, 0, 20));
+        JPanel pAux = new JPanel();
         JPanel panelOpciones = new JPanel(new GridLayout(1, 3));
 
         panelPrincipal.add(new JLabel("<html><font size='20'> Nuevo vehículo </font></html>"));
         creaCampo(panelDatos, new JLabel("Matricula: "), _matricula_vehiculo_text_field);
         creaCampo(panelDatos, new JLabel("Modelo: "), _modelo_vehiculo_text_field);
-        creaCampo(panelDatos, new JLabel("Tipo: "), _tipo_vehiculo_text_field);
+        //creaCampo(panelDatos, new JLabel("Tipo: "), _tipo_vehiculo_text_field);
+        DefaultComboBoxModel<String> tipo_model = new DefaultComboBoxModel<String>();
+        for(TipoCarnet t : TipoCarnet.values()){
+            tipo_model.addElement(t.toString());
+        }
+        JComboBox tipo_vehiculo = new JComboBox(tipo_model);
+        panelDatos.add(tipo_vehiculo);
+
+        creaDesplegable(panelDatos, new JLabel("Tipo: "), tipo_vehiculo);
+
+
 
         panelPrincipal.add(panelDatos);
+        pAux.add(Box.createVerticalStrut(20));
 
         _guardar = new JButton("Guardar");
         panelOpciones.add(_guardar);
@@ -53,7 +66,8 @@ public class GUIAltaVehiculo extends JPanel implements VehiculoObserver{
         _borrar = new JButton("Borrar");
         panelOpciones.add(_borrar);
 
-        panelPrincipal.add(panelOpciones);
+        pAux.add(panelOpciones);
+        panelPrincipal.add(pAux);
 
         add(panelPrincipal);
         setPreferredSize(new Dimension(MainWindow.width, MainWindow.height));
@@ -62,9 +76,14 @@ public class GUIAltaVehiculo extends JPanel implements VehiculoObserver{
 
     private void creaCampo(JPanel panel, JLabel label, JTextPane area_texto) {
         area_texto = new JTextPane();
-        //area_texto.setPreferredSize(new Dimension(100, 30));
+        //area_texto.setPreferredSize(new Dimension(100, 50));
         panel.add(label);
         panel.add(area_texto);
+    }
+
+    private void creaDesplegable(JPanel panel, JLabel label, JComboBox combo) {
+        panel.add(label);
+        panel.add(combo);
     }
 
 
