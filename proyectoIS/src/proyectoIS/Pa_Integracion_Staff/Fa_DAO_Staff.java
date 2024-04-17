7c66359d8caaa51c3770a6eb5c43de8c36a31dac
paBLO7758 <pabloam007@gmail.com>
2024-04-17 12:56:52 +0200

    Pablo y Diego (la parte)
    Basicamente hemos realizado diversas modificaciones enriqueciendo asi el proyecto y basando nuestro trabajo en DAOs.
    un trabajo con un coste de mas de 53 horas en total.
    Os amo grupo <3  <3

--- proyectoIS/src/proyectoIS/Pa_Integracion_Staff/Fa_DAO_Staff.java
+++ proyectoIS/src/proyectoIS/Pa_Integracion_Staff/Fa_DAO_Staff.java
@@ -1,4 +1,44 @@
 package proyectoIS.Pa_Integracion_Staff;
 
-public class Fa_DAO_Staff {
+import proyectoIS.modelo_de_dominio.Staff;
+
+import java.util.List;
+
+public class Fa_DAO_Staff implements Interface_Fa_DAO_Staff_Imp {
+    private Interface_DAO_Staff_Imp iDAOStaffImp;
+
+    @Override
+    public List<Staff> busquedaStaff(String nombre, String apellido1, String apellido2) {
+        return iDAOStaffImp.busquedaStaff(nombre, apellido1, apellido2);
+    }
+
+    @Override
+    public Staff consultaStaff(String dni) {
+        return iDAOStaffImp.consultaStaff(dni);
+    }
+
+    @Override
+    public boolean modificarSatff(Staff staff) {
+        return false;
+    }
+
+    @Override
+    public boolean modificarStaff(Staff staff) {
+        return iDAOStaffImp.modificarStaff(staff);
+    }
+
+    @Override
+    public boolean altaStaff(Staff staff) {
+        return iDAOStaffImp.altaStaff(staff);
+    }
+
+    @Override
+    public boolean bajaStaff(String dni) {
+        return iDAOStaffImp.bajaStaff(dni);
+    }
+
+    @Override
+    public boolean existeStaff(String dni) {
+        return iDAOStaffImp.existeStaff(dni);
+    }
 }
--- proyectoIS/src/proyectoIS/Pa_Integracion_Staff/Interface_DAO_Staff_Imp.java
+++ proyectoIS/src/proyectoIS/Pa_Integracion_Staff/Interface_DAO_Staff_Imp.java
@@ -1,4 +1,19 @@
 package proyectoIS.Pa_Integracion_Staff;
 
+import proyectoIS.modelo_de_dominio.Staff;
+
+import java.util.List;
+
 public interface Interface_DAO_Staff_Imp {
+    public boolean altaStaff(Staff staff);
+
+    public boolean bajaStaff(String dni);
+
+    public boolean modificarStaff(Staff staff);
+
+    public List<Staff> busquedaStaff(String nombre, String apellido1, String Apellido2);
+
+    public Staff consultaStaff(String dni);
+
+    public boolean existeStaff(String dni);
 }
