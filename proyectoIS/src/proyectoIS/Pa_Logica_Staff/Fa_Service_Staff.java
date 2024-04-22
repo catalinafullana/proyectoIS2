487ddfb0938e3ff86d846f413e09713790b4b8b1
paBLO7758 <pabloam007@gmail.com>
2024-04-22 20:10:20 +0200

    Logica de Staff
    Espero que este bien jaja

--- proyectoIS/src/proyectoIS/Pa_Logica_Staff/Fa_Service_Staff.java
+++ proyectoIS/src/proyectoIS/Pa_Logica_Staff/Fa_Service_Staff.java
@@ -1,32 +1,41 @@
 package proyectoIS.Pa_Logica_Staff;
 
+import proyectoIS.Pa_Logica_vehiculo.Interface_Service_Vehiculo;
+import proyectoIS.Pa_Logica_vehiculo.Service_Vehiculo;
+import proyectoIS.misc.TipoCarnet;
 import proyectoIS.modelo_de_dominio.Staff;
+import proyectoIS.modelo_de_dominio.Vehiculo;
 
 import java.util.List;
 
 public class Fa_Service_Staff implements Interface_Fa_Service_Staff{
-    @Override
-    public boolean altaStaff(Staff staff) {
-        return false;
+    private Interface_Service_Staff interface_service_staff;
+
+    public Fa_Service_Staff(){
+        interface_service_staff = new Service_Staff();
     }
 
     @Override
-    public boolean bajaStaff(String dni) {
-        return false;
-    }
-
-    @Override
-    public boolean modificarStaff(Staff staff) {
-        return false;
-    }
-
-    @Override
-    public List<Staff> busquedaStaff(String nombre, String apellido1, String Apellido2) {
-        return null;
+    public List<Staff> busquedaStaff(String nombre, String apellido1, String apellido2) {
+        return interface_service_staff.busquedaStaff(nombre, apellido1, apellido2);
     }
 
     @Override
     public Staff consultaStaff(String dni) {
-        return null;
+        return interface_service_staff.consultaStaff(dni);
     }
+
+    @Override
+    public boolean modificarStaff(Staff staff) {
+        return interface_service_staff.modificarStaff(staff);
+    }
+
+    @Override
+    public boolean altaStaff(Staff staff) {
+        return interface_service_staff.altaStaff(staff);
+    }
+
+    @Override
+    public boolean bajaStaff(String dni) {
+        return interface_service_staff.bajaStaff(dni);}
 }
--- proyectoIS/src/proyectoIS/Pa_Logica_Staff/Service_Staff.java
+++ proyectoIS/src/proyectoIS/Pa_Logica_Staff/Service_Staff.java
@@ -1,32 +1,56 @@
 package proyectoIS.Pa_Logica_Staff;
 
+import proyectoIS.Pa_Integracion_Staff.Fa_DAO_Staff;
+import proyectoIS.Pa_Integracion_Vehiculo.Fa_DAO_Vehiculo;
 import proyectoIS.modelo_de_dominio.Staff;
 
 import java.util.List;
 
 public class Service_Staff implements Interface_Service_Staff{
     @Override
+    public List<Staff> busquedaStaff(String nombre, String apellido1, String apellido2) {
+        Fa_DAO_Staff faDAOStaff = new Fa_DAO_Staff();
+
+        return faDAOStaff.busquedaStaff(nombre, apellido1, apellido2);
+    }
+
+    @Override
+    public Staff consultaStaff(String dni) {
+        Fa_DAO_Staff faDAOStaff = new Fa_DAO_Staff();
+
+        if(!faDAOStaff.existeStaff(dni)) {
+            return faDAOStaff.consultaStaff(dni);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean modificarStaff(Staff staff) {
+        Fa_DAO_Staff faDAOStaff = new Fa_DAO_Staff();
+
+        if(!faDAOStaff.existeStaff(staff.get_dni())){
+            return faDAOStaff.modificarStaff(staff);
+        }
+        return false;
+    }
+
+    @Override
     public boolean altaStaff(Staff staff) {
+        Fa_DAO_Staff faDAOStaff = new Fa_DAO_Staff();
+
+        if(!faDAOStaff.existeStaff(staff.get_dni())){
+            return faDAOStaff.altaStaff(staff);
+        }
         return false;
     }
 
     @Override
     public boolean bajaStaff(String dni) {
+        Fa_DAO_Staff faDAOStaff = new Fa_DAO_Staff();
+
+        if(!faDAOStaff.existeStaff(dni)){
+            return faDAOStaff.bajaStaff(dni);
+        }
         return false;
-    }
-
-    @Override
-    public boolean modificarStaff(Staff staff) {
-        return false;
-    }
-
-    @Override
-    public List<Staff> busquedaStaff(String nombre, String apellido1, String Apellido2) {
-        return null;
-    }
-
-    @Override
-    public Staff consultaStaff(String dni) {
-        return null;
     }
 }
