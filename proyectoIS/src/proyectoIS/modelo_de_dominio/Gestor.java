9a179f006ae55ed7dad8c23429fe33144e6a7f3c
Alai Miranda Blanco Lascurain <alaimbl@gmail.com>
2024-04-23 11:44:49 +0200

    Profesor y gestor eliminados

--- proyectoIS/src/proyectoIS/modelo_de_dominio/Gestor.java
+++
@@ -1,7 +0,0 @@
-package proyectoIS.modelo_de_dominio;
-
-public class Gestor extends Staff{
-    public Gestor(String nombre, String apellido1, String apellido2, String dni, String tlf, String email) {
-        super(nombre, apellido1, apellido2, dni, tlf, email);
-    }
-}
--- proyectoIS/src/proyectoIS/modelo_de_dominio/Staff.java
+++ proyectoIS/src/proyectoIS/modelo_de_dominio/Staff.java
@@ -1,7 +1,22 @@
 package proyectoIS.modelo_de_dominio;
 
+import proyectoIS.misc.Preferencia_clase;
+
 public class Staff extends Persona{
-    public Staff(String nombre, String apellido1, String apellido2, String dni, String tlf, String email) {
+    private Preferencia_clase _preferencia_horario;
+
+    public Staff(String nombre, String apellido1, String apellido2, String dni, String tlf, String email, Preferencia_clase _preferencia_horario) {
         super(nombre, apellido1, apellido2, dni, tlf, email);
+        this._preferencia_horario = _preferencia_horario;
     }
+
+    public Preferencia_clase get_preferencia_horario() {
+        return _preferencia_horario;
+    }
+
+    public void set_preferencia_horario(Preferencia_clase _preferencia_horario) {
+        this._preferencia_horario = _preferencia_horario;
+    }
+
+
 }
